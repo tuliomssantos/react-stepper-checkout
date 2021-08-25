04dc61fff63dbca9e68f774eb7ffdfb92de139ca
Tulio Santos <tulio.santos@grupofleury.com.br>
2021-08-25 18:50:14 -0300

    chore: setup project

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+
+// Please do not use types off of a default export module or else Storybook Docs will suffer.
+// see: https://github.com/storybookjs/storybook/issues/9556
+
+export const Checkout: FC = () => {
+  return <div>React Checkout</div>
+}
